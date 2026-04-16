ba467de4070772490859e3fe14520d1b4e5c8c57
achmadfaiz1 <achmad.f.faiz@gmail.com>
2026-04-16 17:32:15 +0700

    fix build.sh filename

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i.bak "s/REPLACE_WITH_ENV_PASSWORD/$APP_PASSWORD/g" pip_tracker_live.html
-cp pip_tracker_live.html index.html
+sed -i.bak "s/REPLACE_WITH_ENV_PASSWORD/$APP_PASSWORD/g" index.html || true
+echo "Build done"
